a6e4b7151962190379201c3f5ff5db98f587e730
Adam <allblvnk@gmail.com>
2022-03-20 18:47:01 +0000

    First commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("VRDisabler")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VRDisabler")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("B16C8A36-5A2C-4BF8-912A-5726BB039980")]
+
+[assembly: MelonInfo(typeof(DisableVR.VRDisablerMod), "VRDisabler", "1.0.0", "octo")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0"), AssemblyFileVersion("1.0.0.0")]
---
+++ VRDisablerMod.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using Il2CppSystem;
+using Il2CppSystem.Linq;
+using MelonLoader;
+using UnhollowerBaseLib;
+using UnhollowerRuntimeLib;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DisableVR
+{
+    public class VRDisablerMod : MelonMod
+    {
+        bool enabled = false;
+        GameObject vrDisabler;
+
+        public override void OnUpdate()
+        {
+            HandleMouseClick();
+            HandleVrCamera();
+        }
+
+        void HandleVrCamera()
+        {
+            if (Input.GetKeyDown(KeyCode.F11))
+            {
+                if (vrDisabler == null)
+                {
+                    vrDisabler = new GameObject("VRDisabler", new Type[]
+                    {
+                        Il2CppType.Of<VRDisabler>(),
+                        Il2CppType.Of<CameraFly>(),
+                        Il2CppType.Of<Camera>()
+                    });
+                    vrDisabler.GetComponent<Camera>().fieldOfView = 100;
+                }
+                else
+                {
+                    enabled = !enabled;
+                    vrDisabler.SetActive(enabled);
+                }
+            }
+        }
+
+        static void HandleMouseClick()
+        {
+            RaycastHit raycastHit;
+            if (Input.GetKeyDown(KeyCode.Mouse0) &&
+                Physics.Raycast(Camera.current.ScreenPointToRay(Input.mousePosition), out raycastHit))
+            {
+                var transform = raycastHit.transform;
+                var component = transform.GetComponent<GunButton>();
+                if (component != null) component.OnHit(transform.position, KataConfig.I.guns[0]);
+            }
+        }
+    }
+}
